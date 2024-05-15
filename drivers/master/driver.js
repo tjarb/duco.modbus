a4d52b46a20a60997e5da18666679fef510e72df
tjarco <tjarco@delta-elektronika.nl>
2024-05-15 10:08:01 +0200

    Added TCP discovery
    Added a discovery for a TCP master on modbus address 0100 )should be value 17). If so, the discovery will be used for modbus TCP registers.

--- drivers/master/driver.js
+++ drivers/master/driver.js
@@ -68,93 +68,196 @@ async onPair(session){
 		self.log(">>Init pairlistdevices");	
 		client.setTimeout( 1000 );
 		return client.connectTCP(duco_settings.address, { port: duco_settings.port })
-		 .then( async function(data) {	/*Starts in its own environment, we lost local context here*/				
+		 .then( async function(data) {	/*Starts in its own environment, we lost parent context here*/				
 			let node;
 			let devices = [];
+			modbus_TCP = false;	
 			
 			self.log(">>Connected...");	
 			client.setTimeout( 1000 );
 			client.setID(1);		
 			let test = [];
-			for(node=1;node<100;node++)	//try to read sequential devices
-			{	let dev_address = node*10;
-				await client.readInputRegisters (dev_address, 1)	//the system is creating 10 read connections here and fails on the 11th*/
-					.then( data =>{
+			
+			
+			let dev_address = 100;	//check 0100 to be duco master (TCP version)			
+			await client.readInputRegisters (dev_address, 1)	//the system is creating 10 read connections here and fails on the 11th*/
+			{	.then( data =>{
 						let device_type = data.data[0];
-						let device={};
-						//self.log("Read:"+node+"= " + device_type);	
-						
-						switch(device_type){
-						case 0: return;	//no device
-						
-						case 10: 	//Masterbox				
-									device = require('../../lib/master.js');																												
-									break;									
-						case 11: 	//regular valve		
-									device = require('../../lib/regvalve.js');																										
-									break;												
-						case 12: 	//regular valve	
-									device = require('../../lib/co2valve.js');															
-									break;			
-						case 13: 	//regular valve	
-									device = require('../../lib/RHvalve.js');
-									break;			
-						case 14: 	//regular room module without sensors
-									device = require('../../lib/reg_room.js');																				
-									break;			
-						case 15: 	//CO2 room module	
-									device = require('../../lib/CO2room.js');																				
-									break;											
-						case 16: 	//RH room module	
-									device = require('../../lib/RHroom.js');																				
-									break;	
-						case 17: 	//Ventilation grill
-									device = require('../../lib/vent_grill.js');																				
-									break;
-						case 18: 	//Contact
-									device = require('../../lib/contact.js');																				
-									break;
-						case 21: 	//iAV Sensorless
-									device = require('../../lib/iAV_regvalve.js');																				
-									break;
-						case 22: 	//iAV Sensorless
-									device = require('../../lib/iAV_RHvalve.js');																				
-									break;
-						case 23: 	//iAV Sensorless
-									device = require('../../lib/iAV_co2valve.js');																				
-									break;									
+						if(device_type == 17){
+							self.log("System is modbus TCP-based");
+							modbus_TCP = true;
+						}
+						else{
+							self.log("System is modbus RTU-based");
+							modbus_TCP = false;
+						}
+			}	
+			if(modbus_TCP == false){
+				for(node=1;node<100;node++)	//try to read sequential devices
+				{	let dev_address = node*10;
+					await client.readInputRegisters (dev_address, 1)	//the system is creating 10 read connections here and fails on the 11th*/
+						.then( data =>{
+							let device_type = data.data[0];
+							let device={};
+							//self.log("Read:"+node+"= " + device_type);	
+							
+							switch(device_type){
+							case 0: return;	//no device
+							
+							case 10: 	//Masterbox				
+										device = require('../../lib/master.js');																												
+										break;									
+							case 11: 	//regular valve		
+										device = require('../../lib/regvalve.js');																										
+										break;												
+							case 12: 	//regular valve	
+										device = require('../../lib/co2valve.js');															
+										break;			
+							case 13: 	//regular valve	
+										device = require('../../lib/RHvalve.js');
+										break;			
+							case 14: 	//regular room module without sensors
+										device = require('../../lib/reg_room.js');																				
+										break;			
+							case 15: 	//CO2 room module	
+										device = require('../../lib/CO2room.js');																				
+										break;											
+							case 16: 	//RH room module	
+										device = require('../../lib/RHroom.js');																				
+										break;	
+							case 17: 	//Ventilation grill
+										device = require('../../lib/vent_grill.js');																				
+										break;
+							case 18: 	//Contact
+										device = require('../../lib/contact.js');																				
+										break;
+							case 21: 	//iAV Sensorless
+										device = require('../../lib/iAV_regvalve.js');																				
+										break;
+							case 22: 	//iAV Sensorless
+										device = require('../../lib/iAV_RHvalve.js');																				
+										break;
+							case 23: 	//iAV Sensorless
+										device = require('../../lib/iAV_co2valve.js');																				
+										break;									
 
-						case 24 :	//unsupported for now
-						default:	//regular valve	
-									device = require('../../lib/unknown.js');		
-									break;
-						}						
-					
-						devices.push({
-							name: device.name,
-							data: {
-								id: duco_settings.address +": " + duco_settings.port + ":" +node
-							},
-							settings: duco_settings										,										
-							store: {	// Optional: The store is dynamic and persistent storage for your device
-									duco_node: node,
-									duco_type: device_type,
-									duco_lib : device.lib_url
-							},
-							// Optional: These properties overwrite the defaults that you specified in the driver manifest:
-							icon: device.icon,	//"/icon.svg", 	// relative to: /drivers/<driver_id>/assets/
-							capabilities: device.capabilities, //["flow_level", "measure_power.actual", "measure_power.avg", "measure_power.max", "flow_level.low", "flow_level.high", "operational_status", "operational_status.type"],
-							capabilitiesOptions: device.capabilitiesOptions	//{ }																									
+							case 24 :	//unsupported for now
+							default:	//regular valve	
+										device = require('../../lib/unknown.js');		
+										break;
+							}						
+						
+							devices.push({
+								name: device.name,
+								data: {
+									id: duco_settings.address +": " + duco_settings.port + ":" +node
+								},
+								settings: duco_settings										,										
+								store: {	// Optional: The store is dynamic and persistent storage for your device
+										duco_node: node,
+										duco_type: device_type,
+										duco_lib : device.lib_url
+								},
+								// Optional: These properties overwrite the defaults that you specified in the driver manifest:
+								icon: device.icon,	//"/icon.svg", 	// relative to: /drivers/<driver_id>/assets/
+								capabilities: device.capabilities, //["flow_level", "measure_power.actual", "measure_power.avg", "measure_power.max", "flow_level.low", "flow_level.high", "operational_status", "operational_status.type"],
+								capabilitiesOptions: device.capabilitiesOptions	//{ }																									
+							});				
+							self.log("                 Found device: " + device.name + " @ node: "+node);	
+
+						})
+						.catch(function(e){			
+							self.log("Error on "+node+" discovery cycles:"+ e);					//just log
+							//throw new Error("Scan stopped, coud not read all required registers");
 						});				
-						self.log("                 Found device: " + device.name + " @ node: "+node);	
+						
+						
+				}
+			}
+			else	//TCP
+			{	for(node=1;node<100;node++)	//try to read sequential devices
+				{	let dev_address = node*100;
+					await client.readInputRegisters (dev_address, 1)	//the system is creating 10 read connections here and fails on the 11th*/
+						.then( data =>{
+							let device_type = data.data[0];
+							let device={};
+							//self.log("Read:"+node+"= " + device_type);	
+							
+							switch(device_type){
+							case 0: return;	//no device
+							
+							case 17: 	//Masterbox				
+										device = require('../../lib/TCP/master.js');																												
+										break;						
+/*										
+							case 13: 	//regular valve		//sensorless
+										device = require('../../lib/regvalve.js');																										
+										break;												
+							case 16: 	//CO2 valve	
+										device = require('../../lib/co2valve.js');															
+										break;			
+							case 14: 	//RH valve	
+										device = require('../../lib/RHvalve.js');
+										break;			
+							case 8: 	//regular room module without sensors (Batt)
+							case 9: 	//regular room module without sensors (wired)
+										device = require('../../lib/reg_room.js');																				
+										break;			
+							case 12: 	//CO2 room module	
+										device = require('../../lib/CO2room.js');																				
+										break;											
+							case 10: 	//RH room module	
+										device = require('../../lib/RHroom.js');																				
+										break;								
+							case 18: 	//Contact
+										device = require('../../lib/contact.js');																				
+										break;
+						/*	case 17: 	//Ventilation grill
+										device = require('../../lib/vent_grill.js');																				
+										break;
+							case 21: 	//iAV Sensorless
+										device = require('../../lib/iAV_regvalve.js');																				
+										break;
+							case 22: 	//iAV Sensorless
+										device = require('../../lib/iAV_RHvalve.js');																				
+										break;
+							case 23: 	//iAV Sensorless
+										device = require('../../lib/iAV_co2valve.js');																				
+										break;									
+						*/
+							case 24 :	//unsupported for now
+							default:	//regular valve	
+										device = require('../../lib/unknown.js');		
+										break;
+							}						
+						
+							devices.push({
+								name: device.name,
+								data: {
+									id: duco_settings.address +": " + duco_settings.port + ":" +node
+								},
+								settings: duco_settings										,										
+								store: {	// Optional: The store is dynamic and persistent storage for your device
+										duco_node: node,
+										duco_type: device_type,
+										duco_lib : device.lib_url
+								},
+								// Optional: These properties overwrite the defaults that you specified in the driver manifest:
+								icon: device.icon,	//"/icon.svg", 	// relative to: /drivers/<driver_id>/assets/
+								capabilities: device.capabilities, //["flow_level", "measure_power.actual", "measure_power.avg", "measure_power.max", "flow_level.low", "flow_level.high", "operational_status", "operational_status.type"],
+								capabilitiesOptions: device.capabilitiesOptions	//{ }																									
+							});				
+							self.log("                 Found device: " + device.name + " @ node: "+node);	
 
-					})
-					.catch(function(e){			
-						self.log("Error on "+node+" discovery cycles:"+ e);					//just log
-						//throw new Error("Scan stopped, coud not read all required registers");
-					});				
-					
-					
+						})
+						.catch(function(e){			
+							self.log("Error on "+node+" discovery cycles:"+ e);					//just log
+							//throw new Error("Scan stopped, coud not read all required registers");
+						});				
+						
+						
+				}
+		
 			}
 			self.log("devices found =" + devices);
 			return devices;
